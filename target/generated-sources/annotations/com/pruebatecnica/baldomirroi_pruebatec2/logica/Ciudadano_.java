89c019913989fce4cccde6f0cec005906452c1db
Roi Baldomir <roi.baldomir@gmail.com>
2023-11-30 17:08:23 +0100

    Dependencias actualizadas

--- target/generated-sources/annotations/com/pruebatecnica/baldomirroi_pruebatec2/logica/Ciudadano_.java
+++ target/generated-sources/annotations/com/pruebatecnica/baldomirroi_pruebatec2/logica/Ciudadano_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2023-11-30T01:53:40")
+@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2023-11-30T17:04:51")
 @StaticMetamodel(Ciudadano.class)
 public class Ciudadano_ { 
 
--- target/generated-sources/annotations/com/pruebatecnica/baldomirroi_pruebatec2/logica/Turno_.java
+++ target/generated-sources/annotations/com/pruebatecnica/baldomirroi_pruebatec2/logica/Turno_.java
@@ -6,7 +6,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2023-11-30T01:53:40")
+@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2023-11-30T17:04:51")
 @StaticMetamodel(Turno.class)
 public class Turno_ { 
 
